32a1b4eae0425cc8d884bd6c52bb8e974d08967b
Mario Rodriguez <mrodriguez-2023292@kinal.edu.gt>
2024-09-09 10:52:07 -0600

    add: se creo el service de cloudinaryconfig y se creo el controller de la entidad hotel

---
+++ src/main/java/com/braulioneta/gestionHotelera/controller/HotelController.java
@@ -0,0 +1,10 @@
+package com.braulioneta.gestionHotelera.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("localhost:8085/gestionHotelera/v1/auth")
+public class HotelController {
+
+}
---
+++ src/main/java/com/braulioneta/gestionHotelera/service/CloudinaryService.java
@@ -0,0 +1,41 @@
+package com.braulioneta.gestionHotelera.service;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+
+@Service
+public class CloudinaryService {
+
+    @Autowired
+    private Cloudinary cloudinary;
+
+    public Map<String, Object> uploadProfilePicture(MultipartFile file, String folder) throws IOException {
+        String originalFilename = file.getOriginalFilename();
+
+        if(originalFilename == null || originalFilename.isEmpty()){
+            throw new IllegalArgumentException("El archivo no puede ser nulo o estar vacío");
+        } else { 
+            String newName = originalFilename.substring(0, originalFilename.lastIndexOf('.'));
+            String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+            String finalName = newName + "-" + timestamp;
+
+
+            @SuppressWarnings("unchecked")
+            Map<String, Object> uploadResult = (Map<String, Object>) cloudinary.uploader().upload(file.getBytes(), ObjectUtils.asMap(
+                "folder", folder,
+                "public_id", finalName
+            ));
+
+            return uploadResult;
+        }
+    }
+}
