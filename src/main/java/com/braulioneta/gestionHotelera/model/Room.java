6ac7624291592898cc9aaa176a5f7de4dc28b0d7
Mario Rodriguez <mrodriguez-2023292@kinal.edu.gt>
2024-09-09 04:32:43 -0600

    add: se creo la entidad room y la clase status, las se configuraron correctamente

---
+++ src/main/java/com/braulioneta/gestionHotelera/model/Room.java
@@ -0,0 +1,42 @@
+package com.braulioneta.gestionHotelera.model;
+
+import com.braulioneta.gestionHotelera.utils.Status;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String type;
+
+    @NotBlank
+    private Integer capacity;
+
+    @NotBlank
+    private Long price;
+
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.AVAILABLE;
+
+    @NotBlank
+    @ManyToOne
+    private Hotel hotel;
+}
---
+++ src/main/java/com/braulioneta/gestionHotelera/utils/Status.java
@@ -0,0 +1,11 @@
+package com.braulioneta.gestionHotelera.utils;
+
+public enum Status {
+    //Estatus para room(habitacion)
+    AVAILABLE,
+    RESERVED,
+    MAINTENANCE,
+    //Estatus para reservation(reservación)
+    CONFIRMED,
+    CANCELLED
+}
