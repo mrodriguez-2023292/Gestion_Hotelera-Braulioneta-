d96f88d01b332ee808716215f77aca48dd6e4d9c
Mario Rodriguez <mrodriguez-2023292@kinal.edu.gt>
2024-09-09 06:22:41 -0600

    add: se creo el service y junto con su interfaz de la entidad hotel, se configuraron correctamente

---
+++ src/main/java/com/braulioneta/gestionHotelera/service/HotelService.java
@@ -0,0 +1,31 @@
+package com.braulioneta.gestionHotelera.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.braulioneta.gestionHotelera.model.Hotel;
+import com.braulioneta.gestionHotelera.repository.HotelRepository;
+import com.braulioneta.gestionHotelera.service.IService.IHotelService;
+
+@Service
+public class HotelService implements IHotelService{
+    @Autowired
+    HotelRepository hotelRepository;
+
+    @Override
+    public List<Hotel> listHotels(){
+        return hotelRepository.findAll();
+    }
+
+    @Override
+    public Hotel getHotel(Long id){
+        return hotelRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Hotel register(Hotel hotel){
+        return hotelRepository.save(hotel);
+    }
+}
---
+++ src/main/java/com/braulioneta/gestionHotelera/service/IService/IHotelService.java
@@ -0,0 +1,13 @@
+package com.braulioneta.gestionHotelera.service.IService;
+
+import java.util.List;
+
+import com.braulioneta.gestionHotelera.model.Hotel;
+
+public interface IHotelService {
+    List<Hotel> listHotels();
+
+    Hotel getHotel(Long id);
+
+    Hotel register(Hotel hotel);
+}
