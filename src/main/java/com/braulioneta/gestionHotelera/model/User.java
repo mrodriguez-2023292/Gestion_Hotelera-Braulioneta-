41cb1390de3b90ebec8066da27f56d95789a40ad
jgalicia <jgalicia-2022203@kinal.edu.gt>
2024-09-10 17:00:50 -0600

    refactor: se establecio un break para la propiedad Password de la entidad User

--- src/main/java/com/braulioneta/gestionHotelera/model/User.java
+++ src/main/java/com/braulioneta/gestionHotelera/model/User.java
@@ -1,5 +1,48 @@
 package com.braulioneta.gestionHotelera.model;
 
+import com.braulioneta.gestionHotelera.utils.Roles;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long  id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String surname;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    //Password
+    
+    @NotBlank
+    private String phone; 
+
+    @Enumerated(EnumType.STRING)
+    @NotNull
+    private Roles roles;
 }
