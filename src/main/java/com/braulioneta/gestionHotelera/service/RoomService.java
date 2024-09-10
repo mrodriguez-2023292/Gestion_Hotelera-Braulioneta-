9767e2dc1ad31a9d319fce6cca71f58f9b8d70b5
Mario Rodriguez <mrodriguez-2023292@kinal.edu.gt>
2024-09-09 20:24:45 -0600

    feat: se configuro el service de la entidad room

--- src/main/java/com/braulioneta/gestionHotelera/service/RoomService.java
+++ src/main/java/com/braulioneta/gestionHotelera/service/RoomService.java
@@ -1,5 +1,30 @@
 package com.braulioneta.gestionHotelera.service;
 
-public class RoomService {
-    
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.braulioneta.gestionHotelera.model.Room;
+import com.braulioneta.gestionHotelera.repository.RoomRepository;
+import com.braulioneta.gestionHotelera.service.IService.IRoomService;
+
+@Service
+public class RoomService implements IRoomService{
+    @Autowired
+    RoomRepository roomRepository;
+
+    @Override
+    public List<Room> listRooms(){
+        return roomRepository.findAll();
+    }
+
+    @Override
+    public Room getRoom(Long id){
+        return roomRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Room register(Room room){
+        return roomRepository.save(room);
 }
